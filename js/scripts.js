055fe5fdff7b213f566ebe1b35c5dac6ac516425
Brett New and Shradha Pulla <shradha@>
2016-06-20 15:08:02 -0700

    Slight adjustments, problem reading values for decision 2b

--- js/scripts.js
+++ js/scripts.js
@@ -17,26 +17,32 @@ $(document).ready(function(){
     var name = $("input#player1").val();
 
     var newCharacter = new Character(name);
-    var decision1 = $("select#option1").val();
-
 
     $("#decision1").show();
     $("#player-name").text(newCharacter.nameCharacter);
 
-
-    $("#button1").click(function(event) {
-      event.preventDefault();
-      if(decision1 === "1"){
-        $("#decision-2a").show();
-        $("#decision1").hide();
-      } else {
-        $("#decision-2b").show();
-        $("#decision1").hide();
-      }
-    });
-
-
-
-
+  });
+  $("#button1").click(function(event) {
+    event.preventDefault();
+    var decision1 = $("select#option1").val();
+    if (decision1 === "1") {
+      $("#decision-2a").show();
+      $("#decision1").hide();
+    } else if (decision1 === "2") {
+      $("#decision-2b").show();
+      $("#decision1").hide();
+    }
+  });
+  $(".button2").click(function(event) {
+    event.preventDefault();
+    var decision2 = $("select.option2").val();
+    alert(decision2);
+    if ((decision2 === "1") || (decision2 === "4")) {
+      $("#result1").show();
+      $("#decision-2a, #decision-2b").hide();
+    } else if ((decision2 === "2") || (decision2 === "3")) {
+      $("#result2").show();
+      $("#decision-2a, #decision-2b").hide();
+    }
   });
 });
