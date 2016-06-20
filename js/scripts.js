81dc013ebc7f36f217aaea30a8a6c26446851aec
Brett New and Shradha Pulla <shradha@>
2016-06-20 16:37:28 -0700

    Make first btn decision work using the array script code

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,24 @@ $(document).ready(function() {
     newCharacter = new Character (name);
     $("#decision1").show();
     $("#player-name").text(newCharacter.nameCharacter);
+
     $("form#decision-form").submit(function(event) {
       event.preventDefault();
       var decision1 = $("select#option1").val();
       newCharacter.choicesArray.push(decision1);
+      if(newCharacter.choicesArray[0]==="1") {
+        $("#decision-2a").show();
+        $("#decision1").hide();
+      } else {
+        $("#decision-2b").show();
+        $("#decision1").hide();
+      }
+
     $("form#decision2a-form").submit(function(event) {
       event.preventDefault();
       var decision2a = $("select#option2a").val();
       newCharacter.choicesArray.push(decision2a);
+
     $("form#decision2b-form").submit(function(event) {
       event.preventDefault();
       var decision2b = $("select#option2b").val();
