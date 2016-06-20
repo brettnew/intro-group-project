c19386bff145590f637ce836e6545232ef7b166f
Brett New and Shradha Pulla <shradha@>
2016-06-20 13:59:14 -0700

    Add styles to hide divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
 // Back-end
 function Character(name){
   this.nameCharacter = name;
+  // this.decision1 = decision1;
 };
 
+// Character.prototype.adventure = function() {
+//
+// }
 
 
 
 // Front-end
 $(document).ready(function(){
-  $("#---").submit(function(event){
+  $("#intro-form").submit(function(event){
     event.preventDefault();
-    var name = $("input#---").val();
+    var name = $("input#player1").val();
 
     var newCharacter = new Character(name);
+    var decision1 = $("select#option1").val();
 
-    $("#---").show();
+
+    $("#decision1").show();
     $("#---").text(newCharacter.nameCharacter);
 
 
-    if(){
-      $("").show();
-    } else if () {
-      $("").show();
-    }
-    
+    $("#button1").click(function) {
+      if(decision1 === "1"){
+        $("#decision-2a").show();
+      } else {
+        $("#decision-2b").show();
+      }
+    };
+
+
+
+
   });
 });
