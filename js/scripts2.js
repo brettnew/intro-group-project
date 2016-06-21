633b3f55eb0bb476dc04e5f179a8d5b29b0eb924
Alan Denison and Shradha Pulla <alan@>
2016-06-21 08:56:50 -0700

    Change scripts code to link new radio btns

--- js/scripts2.js
+++ js/scripts2.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
   });
   $("form#decision-form").submit(function(event) {
     event.preventDefault();
-    var decision1 = $("select#option1").val();
+    var decision1 = $("input:radio[name=option1]:checked").val();
     if (decision1 === "1") {
       $("#decision-2a").show();
       $("#decision1").hide();
@@ -27,7 +27,7 @@ $(document).ready(function(){
   });
   $("form#decision2a-form").submit(function(event) {
     event.preventDefault();
-    var decision2a = $("select#option2a").val();
+    var decision2a = $("input:radio[name=option2a]:checked").val();
     if (decision2a === "1") {
       $("#decision-3a").show();
       $("#decision-2a").hide();
@@ -38,13 +38,35 @@ $(document).ready(function(){
   });
   $("form#decision2b-form").submit(function(event) {
     event.preventDefault();
-    var decision2b = $("select#option2b").val();
+    var decision2b = $("input:radio[name=option2b]:checked").val();
     if (decision2b === "1") {
       $("#result2").show();
       $("#decision-2b").hide();
     } else if (decision2b === "2") {
       $("#decision-3b").show();
       $("#decision-2b").hide();
+    }
+  });
+  $("form#decision3a-form").submit(function(event) {
+    event.preventDefault();
+    var decision3a = $("input:radio[name=option3a]:checked").val();
+    if (decision3a === "1") {
+      // $("#----")show();
+      $("#decision-3a").hide();
+    } else if (decision3a === "2") {
+      // $("#----").show();
+      $("#decision-3a").hide();
+    }
+  });
+  $("form#decision3b-form").submit(function(event) {
+    event.preventDefault();
+    var decision3b = $("input:radio[name=option3b]:checked").val();
+    if (decision3b === "1") {
+      // $("#----").show();
+      $("#decision-3b").hide();
+    } else if (decision3b === "2") {
+      // $("#----").show();
+      $("#decision-3b").hide();
     }
   });
 });
