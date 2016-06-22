a9864cb3642e5a6b253a9aa9c724790118338f9d
Halle Williams and Shradha Pulla <halle@>
2016-06-22 16:40:15 -0700

    Add slight formatting changes

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,10 @@ $(document).ready(function() {
     var newCharacter = new Character(name, inputtedPetName);
     var personalityType = $("#player-personality").val();
     $(".intro-page").hide();
-    $("#sidebar").show();
+    $("#sidebar").slideToggle(2000);
     $(".player-name").text(newCharacter.nameCharacter);
     $(".petsName").text(newCharacter.petName);
-    $("#decision1").show();
+    $("#decision1").slideToggle(2000);
     if(petType === "dog") {
       $(".dogPet").show();
     } else if (petType === "cat") {
@@ -45,10 +45,10 @@ $(document).ready(function() {
     event.preventDefault();
     var decision1 = $("input:radio[name=option1]:checked").val();
     if (decision1 === "1") {
-      $("#decision-2a").show();
+      $("#decision-2a").fadeToggle().delay(1000);
       $("#decision1").hide();
     } else if (decision1 === "2") {
-      $("#decision-2b").show();
+      $("#decision-2b").fadeToggle().delay(1000);
       $("#decision1").hide();
     }
   });
@@ -56,11 +56,11 @@ $(document).ready(function() {
     event.preventDefault();
     var decision2a = $("input:radio[name=option2a]:checked").val();
     if (decision2a === "1") {
-      $("#decision-3a").show();
+      $("#decision-3a").fadeToggle().delay(1000);
       $("#decision-2a").hide();
     } else if (decision2a === "2") {
-      $("#result1").fadeToggle().delay(1000);
-      $("#reset").show();
+      $("#result1").show(3000);
+      $("#reset").fadeToggle().delay(1000);
       $("#decision-2a").hide();
     }
   });
@@ -71,21 +71,21 @@ $(document).ready(function() {
     var decision2b = $("input:radio[name=option2b]:checked").val();
     if (decision2b === "1") {
       if ((newSpecialItem.item === "lucky rock") && (petType === "lizard")) {
-        $("#reset").show();
-        $("#result9").fadeToggle().delay(1000);
+        $("#reset").fadeToggle().delay(1000);
+        $("#result9").show(3000);
         $("#decision-2b").hide();
       } else {
-        $("#result2").fadeToggle().delay(1000);
+        $("#result2").show(3000);
         $("#decision-2b").hide();
-        $("#reset").show();
+        $("#reset").fadeToggle().delay(1000);
       }
     } else if (decision2b === "2") {
-      $("#segway-2b").show();
+      $("#segway-2b").fadeToggle().delay(1000);
       $("#decision-2b").hide();
     }
   });
   $("#segway2b-btn").click(function() {
-    $("#decision-3b").show();
+    $("#decision-3b").fadeToggle().delay(1000);
     $("#segway-2b").hide();
   })
   $("form#decision3a-form").submit(function(event) {
@@ -94,17 +94,17 @@ $(document).ready(function() {
     var decision3a = $("input:radio[name=option3a]:checked").val();
     if (decision3a === "1") {
       if (newSpecialItem.item === "mini sewing kit") {
-        $("#result8").fadeToggle().delay(1000);
-        $("#reset").show();
+        $("#result8").show(3000);
+        $("#reset").fadeToggle().delay(1000);
         $("#decision-3a").hide();
       } else {
-        $("#result3").fadeToggle().delay(1000);
-        $("#reset").show();
+        $("#result3").show(3000);
+        $("#reset").fadeToggle().delay(1000);
         $("#decision-3a").hide();
       }
     } else if (decision3a === "2") {
-      $("#result5").fadeToggle().delay(1000);
-      $("#reset").show();
+      $("#result5").show(3000);
+      $("#reset").fadeToggle().delay(1000);
       $("#decision-3a").hide();
     }
   });
@@ -113,16 +113,16 @@ $(document).ready(function() {
     var newSpecialItem = new SpecialItem();
     var decision3b = $("input:radio[name=option3b]:checked").val();
     if (decision3b === "1") {
-      $("#decision-3c").show();
+      $("#decision-3c").fadeToggle().delay(1000);
       $("#decision-3b").hide();
     } else if (decision3b === "2") {
       if (newSpecialItem.item === "locket") {
-        $("#result7").fadeToggle().delay(1000);
-        $("#reset").show();
+        $("#result7").show(3000);
+        $("#reset").fadeToggle().delay(1000);
         $("#decision-3b").hide();
       } else {
-        $("#result6").fadeToggle().delay(1000);
-        $("#reset").show();
+        $("#result6").show(3000);
+        $("#reset").fadeToggle().delay(1000);
         $("#decision-3b").hide();
       }
     }
@@ -131,18 +131,18 @@ $(document).ready(function() {
     event.preventDefault();
     var decision3c = $("input:radio[name=option3c]:checked").val();
     if (decision3c === "1") {
-      $("#result4").fadeToggle().delay(1000);
-      $("#reset").show();
+      $("#result4").show(3000);
+      $("#reset").fadeToggle().delay(1000);
       $("#decision-3c").hide();
     } else if (decision3c === "2") {
-      $("#decision-2a").show();
+      $("#decision-2a").fadeToggle().delay(1000);
       $("#decision-3c").hide();
     }
   });
   $(".btn-reset").click(function(){
-    $(".intro-page").show();
+    $(".intro-page").fadeToggle().delay(1000);
     $("#reset").hide();
-    $("#result-total").show();
+    $("#result-total").fadeToggle().delay(1000);
 
     for (i = 1; i<10; i++) {
       if ($("#result" + i).css("display") != "none") {
